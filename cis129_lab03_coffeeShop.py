5bf781eb35f015b44430d4733d8b838e79402d09
zayrahaell <zlopez11@mail.pima.edu>
2024-10-03 22:29:46 -0700

    Update cis129_lab03_coffeeShop.py

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -9,16 +9,22 @@
 #constants
 price_coffee = 5.00
 muffin_price = 4.00
+tea_price = 3.00
+croissants_price = 3.00
 tax_rate = 0.06
 
 #step 1: asking the user for the number of coffees and muffins
 num_coffees = int(input('Number of coffees bought? '))
 num_muffins = int(input('Number of muffins bought? '))
+num_teas = int(input('Number of teas bought? '))
+num_croissant = int(input('Number of croissants bought? '))
 
 #step 2: calculate
 coffee_cost = num_coffees * price_coffee
 muffin_cost = num_muffins * muffin_price
-subtotal = coffee_cost + muffin_cost
+tea_cost = num_teas * tea_price
+croissant_cost = num_croissant * croissants_price
+subtotal = coffee_cost + muffin_cost + tea_cost + croissant_cost
 
 #step 3: calculate tax
 tax = subtotal * tax_rate
@@ -28,9 +34,16 @@ total = subtotal + tax
 print(' ')
 print('*' * 30)
 print('My Coffee and Muffin Shop')
+print(' ')
 print(f'{num_coffees} Coffees at ${price_coffee} each: ${coffee_cost: 6.2f}')
 print(f'{num_muffins} Muffins at ${muffin_price} each: ${muffin_cost: 6.2f}')
+print(f'{num_teas} Teas at ${tea_price} each: ${tea_cost: 6.2f}')
+print(f'{num_croissant} Croissants at ${croissants_price} each: ${croissant_cost: 6.2f}')
 print(f'6% tax: ${tax:6.2f}')
 print('------------------------------')
 print(f'Total: ${total:6.2f}')
 print('*' * 30)
+
+#step 5: thank the user
+print(' ')
+print('Thank you so much for visiting My Coffee Shop!! We hope to see you again soon!')
